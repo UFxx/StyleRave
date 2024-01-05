375ab9182abbf2e06ff367882b6af45627a8d6ad
UFxx <smileprost@gmail.com>
2024-01-05 16:51:58 +0300

    Adapted product page

--- assets/js/product/addedToCartPopup.js
+++ assets/js/product/addedToCartPopup.js
@@ -2,13 +2,18 @@ const addToCartButton = document.querySelector('.product-sizes button');
 const addedToCartPopup = document.querySelector('.added-to-cart');
 
 addToCartButton.addEventListener('click', () => {
-    addedToCartPopup.style.right = '0';
-    addedToCartPopup.style.opacity = 100;
-    
+    addedToCartPopup.style.display = 'block';
+
+    setTimeout(() => {
+        addedToCartPopup.style.right = '0';
+        addedToCartPopup.style.opacity = 100;
+    }, 0)
+
     setTimeout(() => {
         addedToCartPopup.style.right = '-15%';
         setTimeout(() => {
             addedToCartPopup.style.opacity = 0;
+            addedToCartPopup.style.display = 'none';
         }, 300)
     }, 2000)
 })
